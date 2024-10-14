4158b9a231533e44cc8223e3348bb84162345e79
Dandy Wahyudin <75318832+Dandywahyudin@users.noreply.github.com>
2024-10-14 11:06:32 +0700

    Praktikum Pertemuan 4

---
+++ Pertemuan4/Tugas/WindowListenerExample.java
@@ -0,0 +1,55 @@
+package Tugas;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class WindowListenerExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JFrame frame = new JFrame("WindowListener Example");
+		
+		JLabel label = new JLabel("Lakukan Operasi Pada Jendela");
+		label.setBounds(50, 50, 300, 30);
+		
+		frame.addWindowListener(new WindowListener() {
+			public void windowOpened(WindowEvent e) {
+				label.setText("window opened");
+			}
+			
+			public void windowClosing(WindowEvent e) {
+				label.setText("window Closing");
+			}
+			
+			public void windowClosed(WindowEvent e) {
+				System.out.println("Window Closed");
+			}
+			
+			public void windowIconified(WindowEvent e) {
+				label.setText("Window Minimized");
+				
+			}
+			public void windowDeiconified(WindowEvent e) {
+				label.setText("Window Restored.");
+			
+			}
+			public void windowActivated(WindowEvent e) {
+				label.setText("Window Activated");
+			
+			}
+			public void windowDeactivated(WindowEvent e) {
+				label.setText("Window Deactivated");
+			
+			}
+		});
+		
+		frame.add(label);
+		
+		frame.setSize(400, 200);
+		frame.setLayout(null);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
