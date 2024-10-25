4ae4948cdf859157f3199bc6b278fe6d05fb8f42
Dandy Wahyudin <75318832+Dandywahyudin@users.noreply.github.com>
2024-10-25 18:59:42 +0700

    Tugas Pertemuan 5

---
+++ Pertemuan5/Tugas/JLabelHTMLExample.java
@@ -0,0 +1,22 @@
+package Tugas;
+import javax.swing.*;
+
+public class JLabelHTMLExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JFrame frame = new JFrame("JLabel HTML Example");
+		
+		// Membuat JLabel dengan teks HTML
+		JLabel label = new JLabel("<html><b>Bold Text</b>, <i>Italic Text</i>, and <u>Underlined Text</u></html>");
+		
+		// Menambahkan JLabel ke JFrame
+		frame.add(label);
+		
+		// Konfigurasi frame
+		frame.setSize(300, 100);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+
+}
---
+++ Pertemuan5/Tugas/JTableExample.java
@@ -0,0 +1,38 @@
+package Tugas;
+
+import javax.swing.*;
+import javax.swing.table.*;
+
+public class JTableExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Membuat frame
+		JFrame frame = new JFrame("JTable Example");
+		
+		// Membuat tabel dengan data awal dan header kolom
+		String[] columnNames = { "ID", "Name", "Age" };
+		Object[][] data = {
+			{ 1, "John", 25 },
+			{ 2, "Anna", 30 },
+			{ 3, "Mike", 35 },
+		};
+		
+		// Membuat model tabel
+		DefaultTableModel model = new DefaultTableModel(data, columnNames);
+		
+		// Membuat JTable dengan model
+		JTable table = new JTable(model);
+		
+		// Membuat JScrollPane untuk menampung JTable
+		JScrollPane scrollPane = new JScrollPane(table);
+		
+		// Menambahkan JScrollPane ke frame
+		frame.add(scrollPane);
+		
+		// Konfigurasi frame
+		frame.setSize(400, 200);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+}
